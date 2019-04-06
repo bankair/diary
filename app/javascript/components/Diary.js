10dc0dcdbab8113fceb3ed62999e5d5f7435a4d7
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2019-04-06 07:44:55 +0200

    Add Diary component

---
+++ app/javascript/components/Diary.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Diary extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Diary</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Diary
